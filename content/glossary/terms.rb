14a0828f8dc2625a2feaa4c288381b6c31a26192
Adam Phillips <aphillips@scholastic.co.uk>
2013-11-30 22:33:00 +0000

    Added content

--- content/glossary/terms.rb
+++ content/glossary/terms.rb
@@ -8,6 +8,11 @@ DNS stands for Domain Name System.  When we talk about websites, we
 usually refer to them by their domain name, for example facebook.com,
 twitter.com etc. Computers connected in a network, like the internet refer to
 each other in terms of ip addresses, eg 173.252.110.27, 199.16.156.230.
+
+DNS is the mechanism that converts human-friendly domain names into
+machine-friendly ip addresses.
+
+*[DNS]: Domain Name System
 '
   end
 
