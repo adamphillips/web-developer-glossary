2f692271c0b4e540fd3819fca5ae531c3dddba53
Adam Phillips <aphillips@scholastic.co.uk>
2013-11-30 18:26:42 +0000

    Reordered terms

--- content/glossary/terms.rb
+++ content/glossary/terms.rb
@@ -1,6 +1,16 @@
 #Glossary.define_term Glossary::Term.new('HTTP')
 
 Glossary.define do
+  term 'DNS' do
+    subtitle 'Phonebooks for computers'
+    body 'Computers use IP Addresses'
+  end
+
+  term 'HTML' do
+    subtitle 'Hypertext Markup Language'
+    body 'Web pages are written using html'
+  end
+
   term 'HTTP' do
     subtitle 'The glue that binds the web'
     body 'HTTP is the protocol that is used to control the flow of web sessions. It consists of two parts, the request and the response.'
@@ -14,15 +24,5 @@ Glossary.define do
   term 'HTTP Response' do
     subtitle 'Getting something back'
     body 'The counter part to an HTTP Request'
-  end
-
-  term 'DNS' do
-    subtitle 'Phonebooks for computers'
-    body 'Computers use IP Addresses'
-  end
-
-  term 'HTML' do
-    subtitle 'Hypertext Markup Language'
-    body 'Web pages are written using html'
   end
 end
