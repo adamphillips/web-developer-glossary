398c322e75d6834947c25d1cc30f0714c4de7e0a
Adam Phillips <aphillips@scholastic.co.uk>
2013-12-01 02:54:05 +0000

    Tweaked tools content

--- content/glossary/tools.rb
+++ content/glossary/tools.rb
@@ -4,10 +4,11 @@ Glossary.define do
     body '
 The terminal or command-line is an efficient way to talk to your computer.
 '
+    wikipedia_term 'Terminal_emulator'
   end
 
-  term 'Vim' do
-    subtitle 'The undisputed king of text editors'
+  term 'Text Editor' do
+    subtitle 'Writing code'
     body '
 Vim is the only way to edit text.
 '
